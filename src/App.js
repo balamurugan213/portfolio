d787b19bc68312884aca2d864d22c09b84565e62
balamurugan213 <balainfo21@gmail.com>
2022-05-15 00:50:58 +0530

    v2 changes

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import './App.css';
 // import MainText from './component/mainText';
 // import VersionBox from './component/versionBox';
 // import LinkBox from './component/linkBox';
-import { BirdsBackground } from './background';
+import { BirdsBackground } from './v2/background';
 import VersionTextTwo from './v2/versionText2';
 import HeaderTitle from './v2/headerTitle';
 
 function App() {
   return (
     <div className="App" >
-     <BirdsBackground>
-       <div className="">
-
-  
-     <VersionTextTwo>
-
-     </VersionTextTwo>
-    <HeaderTitle></HeaderTitle>
-       </div>
-     </BirdsBackground>
+    <BirdsBackground>
+      <div className="">
+      <VersionTextTwo/>
+      <HeaderTitle/>
+      </div>
+    </BirdsBackground>
+     {/* <VersionBox></VersionBox>
+        <MainText></MainText> */}
     </div>
   );
 }
--- src/v2/headerTitle.js
+++ src/v2/headerTitle.js
@@ -1,4 +1,5 @@
 import styles from '../versiontwo.module.css'
+import { useMediaQuery } from 'react-responsive'
 
 const HeaderTitle = () => {
     return ( 
@@ -10,12 +11,32 @@ const HeaderTitle = () => {
             <h1>
                 I am Balamurugan213
             </h1>
-            <p>
-            A developer who is curious to learn new technologies every day <br/>and use it to its advantage with the things I develop.
-            </p>
+        <ContentDetail></ContentDetail>
+            {/* <p>
+            A developer who is curious to learn new technologies every day and use it to its advantage with the things I develop.
+            </p> */}
         </div>
         </>
     );
+}
+
+
+const ContentDetail=()=>{
+    if(useMediaQuery({ maxWidth: 670 }))
+    {
+        return(
+            <p>
+            A developer who is curious to learn new technologies every day and use it to its advantage with the things I develop.
+            </p>
+        )
+    }
+    else{
+        return(
+            <p>
+            A developer who is curious to learn new technologies every day <br/> and use it to its advantage with the things I develop.
+            </p>
+        )
+    }
 }
 
 export default HeaderTitle;
