f9797cab1caff21fd86b8a4ccf02506f88abf98f
balamurugan213 <balainfo21@gmail.com>
2022-05-15 18:37:46 +0530

    v2-adding links

--- src/v2/headerTitle.js
+++ src/v2/headerTitle.js
@@ -1,9 +1,11 @@
 import styles from '../versiontwo.module.css'
 import { useMediaQuery } from 'react-responsive'
+import logo from '../img/icons/github-brands.svg';
 
 const HeaderTitle = () => {
     return ( 
         <>
+       
         <div className={styles.headertext} style={{color:'black'}}>
             <h3>
                 Hi There
@@ -12,10 +14,17 @@ const HeaderTitle = () => {
                 I am Balamurugan213
             </h1>
         <ContentDetail></ContentDetail>
-            {/* <p>
-            A developer who is curious to learn new technologies every day and use it to its advantage with the things I develop.
-            </p> */}
+        <div class="social-menu">
+        <ul>
+            <li><a href="https://github.com/balamurugan213" target="blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.instagram.com/balamurugan213_" target="blank"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/balamurugan213/" target="blank"><i class="fab fa-linkedin-in"></i></a></li>
+            {/* <li><a href="https://codepen.io/sanketbodke"><i class="fab fa-codepen" target="blank"></i></a></li> */}
+        </ul>
+    </div>
+        
         </div>
+        
         </>
     );
 }
